4626ff0a51208061217eb25bfd4541f370c9db45
Yujiin <jiin4083@naver.com>
2024-12-17 17:16:56 +0900

    useEffect done

--- project1/src/App.js
+++ project1/src/App.js
@@ -1,7 +1,8 @@
-import { use, useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import Controller from './component/Controller';
 import Viewer from './component/Viewer';
+import Even from './component/Even';
 
 // 부모 -> 자식이기에 App.js에 카운터 앱에 사용할 State 작성
 function App() {
@@ -9,11 +10,17 @@ function App() {
   const handleSetCount = (value) => {
     setCount(count + value)
   }
+
+  useEffect(() => {
+    console.log("count 업데이트: ", count)
+  }, [count])
+
   return (
     <div className="App">
       <h1>Simple Counter</h1>
       <section>
         <Viewer count = {count}/>
+        {count % 2 === 0 && <Even/>}
       </section>
       <section>
         <Controller handleSetCount = {handleSetCount}/>
