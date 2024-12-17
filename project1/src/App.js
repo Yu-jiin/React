39bfd21412783adf5a0ab95c674e24120ca2c862
Yujiin <jiin4083@naver.com>
2024-12-17 16:28:53 +0900

    Simple Counter project done

--- project1/src/App.js
+++ project1/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import { use, useState } from 'react';
 import './App.css';
+import Controller from './component/Controller';
+import Viewer from './component/Viewer';
 
+// 부모 -> 자식이기에 App.js에 카운터 앱에 사용할 State 작성
 function App() {
+  const [count, setCount] = useState(0)
+  const handleSetCount = (value) => {
+    setCount(count + value)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Simple Counter</h1>
+      <section>
+        <Viewer count = {count}/>
+      </section>
+      <section>
+        <Controller handleSetCount = {handleSetCount}/>
+      </section>
     </div>
   );
 }
 
 export default App;
+
