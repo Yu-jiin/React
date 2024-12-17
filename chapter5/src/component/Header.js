4d5dc8614a257455b9fc61d18ab08c3b4c5d96cd
Yujiin <jiin4083@naver.com>
2024-12-17 14:31:44 +0900

    chapter5 done

--- chapter5/src/component/Header.js
+++ chapter5/src/component/Header.js
@@ -1,7 +1,16 @@
+import {useState} from "react";
+
 function Header() {
+  const [count, setCount] = useState(0)
+  const onIncrease = () => {
+    setCount(count + 1)
+  }
+
   return (
     <header>
       <h1>header</h1>
+      <h2>{count}</h2>
+      <button onClick={onIncrease}>+</button>
     </header>
   )
 }
