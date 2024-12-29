af12f6ed4d997624f497237d730260f23f5e075f
Yujiin <jiin4083@naver.com>
2024-12-29 21:27:50 +0900

    app.css 수정 done

--- todo_project/src/component/TodoList.js
+++ todo_project/src/component/TodoList.js
@@ -1,3 +1,4 @@
+import TodoItem from "./TodoItem"
 import "./TodoList.css"
 
 const TodoList = () => {
@@ -5,6 +6,11 @@ const TodoList = () => {
         <div className="TodoList">
             <h4>Todo List 🍁</h4>
             <input className="searchbar" placeholder="검색어를 입력하세요" />
+            <div className="list_wrapper">
+                <TodoItem/>
+                <TodoItem/>
+                <TodoItem/>
+            </div>
         </div>
     )
 }
