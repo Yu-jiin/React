5da95fb59419a7c8f7ed3fa0c469e6130f30ff39
Yujiin <jiin4083@naver.com>
2025-03-13 21:03:39 +0900

    reactelement

--- ssafy_react/src/main.ts
+++ ssafy_react/src/main.ts
@@ -1,1 +1,37 @@
 console.log('hello react')
+
+import * as React from 'react'
+import * as ReactDom from 'react-dom/client'
+
+console.log(React.version)
+
+// React.ReactNode
+
+
+
+// React.ReactElement [TypeScript]
+//  JavaScript Library
+const reactElement = React.createElement('button', {
+    type: 'button',
+    className: 'button primary-button',
+    children: '리액트 버전 확인',
+}) 
+// == <button><button/>     ReactElement = Object
+
+// DOM API
+const rootElement = document.getElementById('root')
+// null 일때를 방지 그래서 if문
+if(rootElement){
+    const reactDomRoot = ReactDom.createRoot(rootElement)
+    console.log(reactDomRoot.render(reactElement))
+}
+
+
+console.log(reactElement)
+console.log(ReactDom.createRoot)
+
+
+
+
+
+// React.Component 
--- ssafy_react/vite.config.ts
+++ ssafy_react/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite"
 
 const viteConfig = defineConfig({
+    base: '/',
     server: {
         port: 3000,
     },
